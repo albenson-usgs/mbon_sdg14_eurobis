036ac69cd24a929c2c903c659dbc5ad38a7fe278
Abby Benson <albenson@usgs.gov>
2017-06-26 16:46:00 -0600

    Phylum abundances, unit adjustments

--- EurOBIS_site.R
+++ EurOBIS_site.R
@@ -6,10 +6,10 @@ library(dplyr)
 
 
 #import files downloaded from EurOBIS IPT
-imr_fish_eggs_survey_occurrence <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-imr_fish_eggs_survey-v1.1/occurrence.txt",
-                                              "\t", escape_double = FALSE, trim_ws = TRUE)
-imr_fish_eggs_survey_measurementorfact <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-imr_fish_eggs_survey-v1.1/measurementorfact.txt",
-                                                     "\t", escape_double = FALSE, trim_ws = TRUE)
+# imr_fish_eggs_survey_occurrence <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-imr_fish_eggs_survey-v1.1/occurrence.txt",
+#                                               "\t", escape_double = FALSE, trim_ws = TRUE)
+# imr_fish_eggs_survey_measurementorfact <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-imr_fish_eggs_survey-v1.1/measurementorfact.txt",
+#                                                      "\t", escape_double = FALSE, trim_ws = TRUE)
 imr_juvenile_fish_monitoring_occurrence <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-imr_juveline_fish_monitoring-v1.1/occurrence.txt",
                                                       "\t", escape_double = FALSE, trim_ws = TRUE)
 imr_juvenile_fish_monitoring_measurementorfact <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-imr_juveline_fish_monitoring-v1.1/measurementorfact.txt",
@@ -39,7 +39,7 @@ imr_zoopl_norwsea_measurementorfact <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG1
 # nsbp_cochrane_measurementorfact <- read_delim("~/OBIS/MBONs/GEOBON MBON/SDG14Project/dwca-nsbp_cochrane-v1.1/measurementorfact.txt",
 #                                               "\t", escape_double = FALSE, trim_ws = TRUE)
 
-occurrence_combined <- rbind(imr_fish_eggs_survey_occurrence, imr_juvenile_fish_monitoring_occurrence, imr_mareano_beamtrawl_occurrence, 
+occurrence_combined <- rbind(imr_juvenile_fish_monitoring_occurrence, imr_mareano_beamtrawl_occurrence, 
                              imr_mareano_grab_occurrence, imr_mareano_rpsledge_occurrence, imr_zoopl_northsea_occurrence, 
                              imr_zoopl_norwsea_occurrence)
 
@@ -58,7 +58,7 @@ o %>%
   knitr::kable()
 
 # Combining all the measurement or fact files into one file
-mof_combined <- rbind(imr_fish_eggs_survey_measurementorfact, imr_juvenile_fish_monitoring_measurementorfact, imr_mareano_beamtrawl_measurementorfact, 
+mof_combined <- rbind(imr_juvenile_fish_monitoring_measurementorfact, imr_mareano_beamtrawl_measurementorfact, 
                              imr_mareano_grab_measurementorfact, imr_mareano_rpsledge_measurementorfact, imr_zoopl_northsea_measurementorfact, 
                              imr_zoopl_norwsea_measurementorfact)
 mof_abundanceonly <- mof_combined[which(mof_combined$measurementType== "Abundance"),]
@@ -66,9 +66,18 @@ mof_abundanceonly <- mof_combined[which(mof_combined$measurementType== "Abundanc
 # Merging the measurement or fact abundance only table with the occurrences
 oc_mof <- left_join(occurrence_combined, mof_abundanceonly, by = "id")
 
+## To make the abundances more equivalent converted: 
+# number per 100 square meter to number per square meter
+oc_mof$measurementValue <- ifelse (oc_mof$measurementUnit == "Number per 100 square meter", oc_mof$measurementValue/100, oc_mof$measurementValue*1)
+# oc_mof$measurementUnit <- ifelse(oc_mof$measurementUnit == "Number per 100 square meter", "Number per square meter", ) # not sure what to put in the else
+# number per nautical mile to number per meter
+oc_mof$measurementValue <- ifelse(oc_mof$measurementUnit == "number per nautic mile with Pelagic trawl, Opening: 18x18m", oc_mof$measurementValue/1852,
+                                  oc_mof$measurementValue*1)
+oc_mof$measurementValue <- ifelse(oc_mof$measurementUnit == "number per nautic mile with Pelagic trawl, Opening: 29x29m", oc_mof$measurementValue/1852,
+                                  oc_mof$measurementValue*1)
+
 # Plot over time the abundance for each taxonomic group, just a test at first to see what it looks like need to keep in mind units
 annelida_abundance <- oc_mof[which(oc_mof$phylum == "Annelida"),]
-
 annelida_abundance %>%
   group_by(year) %>%
   summarize(avg = mean(measurementValue)) %>%
@@ -76,30 +85,147 @@ annelida_abundance %>%
   knitr::kable()
 
 arthropoda_abundance <- oc_mof[which(oc_mof$phylum == "Arthropoda"),]
-
 arthropoda_abundance %>%
   group_by(year) %>%
   summarize(avg = mean(measurementValue)) %>%
   arrange(avg) %>%
   knitr::kable()
 
-mollusca_abundance <- oc_mof[which(oc_mof$phylum == "Mollusca"),]
-
+mollusca_abundance <- oc_mof[which(oc_mof$phylum == "Mollusca"),] #should I break up mollusca like I did with chordata?
 mollusca_abundance %>%
   group_by(year) %>%
   summarize(avg = mean(measurementValue)) %>%
   arrange(avg) %>%
   knitr::kable()
 
-enchinodermata_abundance <- oc_mof[which(oc_mof$phylum == "Echinodermata"),]
-
+enchinodermata_abundance <- oc_mof[which(oc_mof$phylum == "Echinodermata"),] #echinoderms/sea stars/sea urchins/sand dollars/sea cucumbers
 enchinodermata_abundance %>%
   group_by(year) %>%
   summarize(avg = mean(measurementValue)) %>%
   arrange(avg) %>%
   knitr::kable()
 
-# Break up Chordata to smaller taxonomic levels
+chaetognatha_abundance <- oc_mof[which(oc_mof$phylum == "Chaetognatha"),] # arrow worms
+chaetognatha_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+cnidaria_abundance <- oc_mof[which(oc_mof$phylum == "Cnidaria"),] #jellyfish/corals/hydrozoans/box jellies
+cnidaria_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+porifera_abundance <- oc_mof[which(oc_mof$phylum == "Porifera"),] #sponges
+porifera_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+sipuncula_abundance <- oc_mof[which(oc_mof$phylum == "Sipuncula"),] #peanut worms
+sipuncula_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+bryozoa_abundance <- oc_mof[which(oc_mof$phylum == "Bryozoa"),] #sea mats
+bryozoa_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+brachiopoda_abundance <- oc_mof[which(oc_mof$phylum == "Brachiopoda"),] #lamp shells
+brachiopoda_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+nemertea_abundance <- oc_mof[which(oc_mof$phylum == "Nemertea"),] #ribbon worms
+nemertea_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+nematoda_abundance <- oc_mof[which(oc_mof$phylum == "Nematoda"),] #round worms
+nematoda_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+hemichordata_abundance <- oc_mof[which(oc_mof$phylum == "Hemichordata"),] #hemichordates (acorn worms)
+hemichordata_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+ctenophora_abundance <- oc_mof[which(oc_mof$phylum == "Ctenophora"),] #comb jellies
+ctenophora_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+sarcomastigophora_abundance <- oc_mof[which(oc_mof$phylum == "Sarcomastigophora"),] #protozoans
+sarcomastigophora_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+foraminifera_abundance <- oc_mof[which(oc_mof$phylum == "Foraminifera"),] #forams (amoeboid protists)
+foraminifera_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+radiozoa_abundance <- oc_mof[which(oc_mof$phylum == "Radiozoa"),] #radiolarians
+radiozoa_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+platyhelminthes_abundance <- oc_mof[which(oc_mof$phylum == "Platyhelminthes"),] #flatworms
+platyhelminthes_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+haptophyta_abundance <- oc_mof[which(oc_mof$phylum == "Haptophyta"),] #algae
+haptophyta_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+rotifera_abundance <- oc_mof[which(oc_mof$phylum == "Rotifera"),] #rotifers (wheel animals)
+rotifera_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+cephalorhyncha_abundance <- oc_mof[which(oc_mof$phylum == "Cephalorhyncha"),]
+cephalorhyncha_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+
+# Break up Chordata to smaller taxonomic level (class), leaving out Holocephali and Myxini because too few observations
 chordata_abundance <- oc_mof[which(oc_mof$phylum == "Chordata"),]
 chor <- chordata_abundance
 chor %>% 
@@ -109,7 +235,31 @@ chor %>%
   arrange(desc(n)) %>%
   knitr::kable()
 
-# Try to make abundances equivalent (so convert 100 m to 1 m units)
+actinopterygii_abundance <- oc_mof[which(oc_mof$class == "Actinopterygii" | oc_mof$class == "Actinopteri"),] #ray finned fish
+actinopterygii_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+tunicate_abundance <- chordata_abundance[which(chordata_abundance$class == "Appendicularia" | chordata_abundance$class == "Ascidiacea" | 
+                                                 chordata_abundance$class == "Thaliacea"),] #tunicates
+tunicate_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+elasmobranchiii_abundance <- oc_mof[which(oc_mof$class == "Elasmobranchii"),]
+elasmobranchiii_abundance %>%
+  group_by(year) %>%
+  summarize(avg = mean(measurementValue)) %>%
+  arrange(avg) %>%
+  knitr::kable()
+
+
+
+
 # Try to take into account level of effort somehow? Number of collection days?
   
 
